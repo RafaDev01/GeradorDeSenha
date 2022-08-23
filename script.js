652826aab28b8fe4f51350760a72b007285ba3df
Rafael Nascimento <rflwwe123@hotmail.com>
2022-08-23 00:33:12 -0300

    alguns coments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//direcionando elementos
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
@@ -9,8 +10,11 @@ let containerPassword = document.querySelector("#container-password");
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@";
 let novaSenha = "";
 
+//sizePassword = o valor de slider element
 sizePassword.innerHTML = sliderElement.value;
 
+
+//id slider oninput chama a function -> tamanho da senha em html
 slider.oninput = function(){
   sizePassword.innerHTML = this.value;
 }
@@ -18,7 +22,9 @@ slider.oninput = function(){
 function generatePassword(){
   
   let pass = "";
+  //n = passa o tamanho da variavel charset 
   for(let i=0, n = charset.length; i < sliderElement.value; ++i){
+    //atribui ao pass o charAt(M.floor para arredondar e random para gerar um numero aleatório * n para cobrir todo o tamanho do charset)
     pass += charset.charAt(Math.floor(Math.random() * n));
   }
   containerPassword.classList.remove("hide");
